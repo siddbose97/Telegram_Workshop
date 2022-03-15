d50f9e97e92baea5a8aa103e8a00d13b4d90db85
Siddhartha Bose <siddbose97@gmail.com>
2022-03-15 19:26:22 +0800

    edited slides

--- dummy.py
+++ dummy.py
@@ -1,108 +1,70 @@
 import telegram
 from telegram.ext import Updater, ConversationHandler, CommandHandler, MessageHandler, Filters
 
-# The API Key we received for our bot
-API_KEY = '5062654174:AAEg0b-JUF46drA87A4jnnmRohav0LyUBYA'
-updater = Updater(API_KEY)
-dispatcher = updater.dispatcher
-
-FIRSTSTEP, SECONDSTEP, THIRDSTEP = range(3)
-
-     
-def help(update_obj, context):
-    try:
-        help_string = """
-This is a dummy bot! Thanks for coming
-        """
-
-        update_obj.message.reply_text(help_string)
-        return ConversationHandler.END
-
-    except Exception as e:
-        cancel(update_obj, context)
-        return ConversationHandler.END
-#=================================================================================================================
-
 class Human:
     def __init__(self, name):
         self.name = name
         self.age = 0
         self.gender = ""
 
-humans = {}
+humans_dict = {}
 
+API_KEY = "5062654174:AAEg0b-JUF46drA87A4jnnmRohav0LyUBYA"
+updater = Updater(API_KEY)
+dispatcher = updater.dispatcher
 
-# The entry function
+FIRSTSTEP = 1
+SECONDSTEP = 2
+THIRDSTEP = 3
+
 def start(update_obj, context):
+  update_obj.message.reply_text("Hello there, what is your name?")
+  return FIRSTSTEP
+
+def ask_age_step(update_obj, context):
+  chat_id = update_obj.message.chat_id
+  name = update_obj.message.text
+  humans_dict[chat_id] = Human(name)
+
+  update_obj.message.reply_text("How old are you?")
+  return SECONDSTEP
   
-    try:
-        
-        chat_id = update_obj.message.chat_id
-        update_obj.message.reply_text("Hello there, what is your name?")
-        return FIRSTSTEP
-    except Exception:
-        cancel(update_obj, context)
+def ask_gender_step(update_obj, context):
+  chat_id = update_obj.message.chat_id
+  age = update_obj.message.text
+  user = humans_dict[chat_id]
+  user.age = age
 
+  list1 = [[telegram.KeyboardButton(text="Male")],[telegram.KeyboardButton(text="Female")]]
+  
+  kb = telegram.ReplyKeyboardMarkup(keyboard=list1,resize_keyboard = True, one_time_keyboard = True)
 
-def name_step(update_obj, context):
-      
-    try:
-        
-        chat_id = update_obj.message.chat_id
-        msg = update_obj.message.text
-        humans[chat_id] = Human(msg)
-        list1 = [[telegram.KeyboardButton(text=gender)] for gender in ["Male", "Female"]]
-        kb = telegram.ReplyKeyboardMarkup(keyboard=list1,resize_keyboard = True, one_time_keyboard = True)
+  update_obj.message.reply_text("What is your gender?",reply_markup=kb)
+  
+  return THIRDSTEP
 
-        update_obj.message.reply_text("What is your gender?",reply_markup=kb)
-    # go to the Batallion state
-        return SECONDSTEP
-    except Exception as e:
-        cancel(update_obj, context)
-
-def gender_step(update_obj, context):
-      
-    try:
-        chat_id = update_obj.message.chat_id
-        msg = update_obj.message.text
-        humans[chat_id].gender = msg
-        
-        update_obj.message.reply_text("How old are you?")
-        return THIRDSTEP
-    except Exception as e:
-        cancel(update_obj, context)
-
-def end(update_obj, context):
-    try:
-        chat_id = update_obj.message.chat_id
-        msg = update_obj.message.text
-        humans[chat_id].age = msg
-        
-        update_obj.message.reply_text(
-            f"Thank you {humans[chat_id].name}, you are a {humans[chat_id].gender} and {humans[chat_id].age} years old", reply_markup=telegram.ReplyKeyboardRemove()
-        )
-        return ConversationHandler.END
-    except Exception as e:        
-        cancel(update_obj, context)
-        return ConversationHandler.END 
-
-
-def cancel(update_obj, context):
-    # get the user's first name
-    update_obj.message.reply_text(
-        f"Okay, bad input, press /start to try again",reply_markup=telegram.ReplyKeyboardRemove())
-    return ConversationHandler.END       
+def output_info_step(update_obj, context):
+  chat_id = update_obj.message.chat_id
+  gender = update_obj.message.text
+  user = humans_dict[chat_id]
+  user.gender = gender
+  
+  update_obj.message.reply_text(
+      f"Thank you {user.name}, you are a {user.gender} and {user.age} years old", reply_markup=telegram.ReplyKeyboardRemove()
+  )
+  return ConversationHandler.END
 
 def main():
-
+    
     handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start),CommandHandler('help', help)],
+        entry_points=[CommandHandler('start', start)],
         states={
-                FIRSTSTEP: [MessageHandler(Filters.text, name_step)],
-                SECONDSTEP: [MessageHandler(Filters.text, gender_step)],
-                THIRDSTEP: [MessageHandler(Filters.text, end)],
+          FIRSTSTEP: [MessageHandler(Filters.text, ask_age_step)],
+          SECONDSTEP: [MessageHandler(Filters.text, ask_gender_step)],
+          THIRDSTEP: [MessageHandler(Filters.text, output_info_step)]
+          
         },
-        fallbacks=[CommandHandler('cancel', end)],
+        fallbacks=[],
         )
     # add the handler to the dispatcher
     dispatcher.add_handler(handler)
@@ -114,8 +76,5 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
 
 
